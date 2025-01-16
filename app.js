809856292ff622838a2970fe402806d1994eb5a3
Abhiraj Yadav <abhiraj100right@gmail.com>
2025-01-16 07:28:17 +0530

    add github data

--- app.js
+++ app.js
@@ -4,6 +4,44 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+const githubData = {
+    "login": "abhiraj100",
+    "id": 102871390,
+    "node_id": "U_kgDOBiGxXg",
+    "avatar_url": "https://avatars.githubusercontent.com/u/102871390?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/abhiraj100",
+    "html_url": "https://github.com/abhiraj100",
+    "followers_url": "https://api.github.com/users/abhiraj100/followers",
+    "following_url": "https://api.github.com/users/abhiraj100/following{/other_user}",
+    "gists_url": "https://api.github.com/users/abhiraj100/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/abhiraj100/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/abhiraj100/subscriptions",
+    "organizations_url": "https://api.github.com/users/abhiraj100/orgs",
+    "repos_url": "https://api.github.com/users/abhiraj100/repos",
+    "events_url": "https://api.github.com/users/abhiraj100/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/abhiraj100/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "ABHIRAJ YADAV",
+    "company": "Student In VIT",
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 56,
+    "public_gists": 1,
+    "followers": 1,
+    "following": 2,
+    "created_at": "2022-04-02T16:28:09Z",
+    "updated_at": "2024-10-19T19:14:20Z"
+    }
+
+    
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -19,6 +57,10 @@ app.get("/login", (req, res) => {
 app.get("/youtube", (req, res) => {
   res.send("<h2>Chai aur code</h2>");
 });
+
+app.get("/github", (req, res) => {
+    res.json(githubData);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening at PORT: ${PORT}`);
